6e6f280b1b0b429feb716879049a13b38c761aeb
Zhanserik Suyunbayev <zhanswift@gmail.com>
2023-05-03 21:46:35 -0500

    Initial commit

---
+++ Simpsons Viewer/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  Simpsons Viewer
+//
+//  Created by Zhanserik Suyunbayev on 5/3/23.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate, UINavigationControllerDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        let controller = RootTableViewController()
+        let navigationVC = UINavigationController(rootViewController: controller)
+        navigationVC.delegate = self
+        window?.rootViewController = navigationVC
+        
+        return true
+    }
+
+}
+
---
+++ Simpsons Viewer/RootTableViewController.swift
@@ -0,0 +1,17 @@
+//
+//  RootTableViewController.swift
+//  Viewer
+//
+//  Created by Zhanserik Suyunbayev on 5/3/23.
+//
+
+import UIKit
+
+class RootTableViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .white
+    }
+}
